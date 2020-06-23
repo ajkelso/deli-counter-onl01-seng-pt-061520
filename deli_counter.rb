44fb74120e24111e6c0c608391c382d633e2c79b
A. Kelso <ajkelso@gmail.com>
2020-06-23 02:07:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,7 +14,15 @@
     #end
 #end
 
-
+def line(katz_deli)
+  if katz_deli.length > 0  
+  list =[]
+  katz_deli.each_with_index {|val, index| list.push("#{index + 1}. #{val} ")}
+      puts "The line is currently: #{list.join(" ")}"
+    else
+      puts "The line is currently empty."
+    end
+end
 
 def take_a_number(katz_deli, name)
     katz_deli.push(name)
